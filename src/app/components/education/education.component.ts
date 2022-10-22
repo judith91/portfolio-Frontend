210b4ba3f1d64b5119543dd174e600672c38b76a
judithleonardo <judith.leonardao@gmail.com>
2022-10-22 17:40:15 -0300

    banner

--- src/app/components/education/education.component.ts
+++ src/app/components/education/education.component.ts
@@ -7,9 +7,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EducationComponent implements OnInit {
 
+  educations: any = [{title:"Isnstituto del Profesorado Gral. San Martín f- 49. Nivel Superior.", date:"March 2010 - August 2017", description:"Profesorado de educación secundaria en lengua y literatura"}]
+
   constructor() { }
 
   ngOnInit(): void {
+    
   }
 
 }
--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -6,10 +6,16 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./header.component.css']
 })
 export class HeaderComponent implements OnInit {
+  showBanner: boolean = false
+  isLogin: boolean = false
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  getShowBanner(): void {
+    this.showBanner = !this.showBanner
   }
 
 }
